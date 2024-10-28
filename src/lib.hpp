4f4b9f0657ce6686163ade0d721decd9ad00bc28
Anton Plusnin <anton@vipset.com>
2024-10-28 13:31:59 +0300

    done

--- src/lib.hpp
+++ src/lib.hpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <string>
 
+
 struct regular_expr {
     std::vector<int> prefix; // minimal length of word, that s[1 .. prefix[i]] is [x]^i
     std::vector<int> whole; // whole[j] = j if it can be [x]^j
@@ -8,5 +9,11 @@ struct regular_expr {
 
     regular_expr(size_t length);
 };
+
+regular_expr sum(const regular_expr& lhs, const regular_expr& rhs, size_t length);
+
+regular_expr concat(const regular_expr& lhs, const regular_expr& rhs, size_t length);
+
+regular_expr clini(const regular_expr& re, size_t length);
 
 int solve(std::string str, char x, size_t length);
--- test/test.cpp
+++ test/test.cpp
@@ -12,3 +12,47 @@ TEST(regexpr, sample2) {
 TEST(regexpr, cool) {
     ASSERT_EQ(solve("bb.*", 'b', 5), 6);
 }
+
+TEST(regexpr, sum) {
+    regular_expr a(2);
+    regular_expr b(2);
+    a.prefix[0] = 1;
+    a.prefix[1] = 2;
+    b.whole[0] = 0;
+    b.whole[1] = 1;
+    b.prefix[0] = 0;
+    b.prefix[1] = 3;
+    regular_expr result = sum(a, b, 2);
+    std::vector<int> test = {0, 2};
+    ASSERT_EQ(result.prefix, test);
+    ASSERT_EQ(result.whole, b.whole);
+}
+
+TEST(regexpr, concat) {
+    regular_expr a(2);
+    regular_expr b(2);
+    a.prefix[0] = 0;
+    a.prefix[1] = 2;
+    a.whole[0] = 0;
+    b.whole[0] = 0;
+    b.whole[1] = 1;
+    b.prefix[0] = 0;
+    b.prefix[1] = 3;
+    regular_expr result = concat(a, b, 2);
+    std::vector<int> test_prefix = {0, 2};
+    std::vector<int> test_whole = {0, 1};
+    ASSERT_EQ(result.prefix, test_prefix);
+    ASSERT_EQ(result.whole, test_whole);
+}
+
+TEST(regexpr, clini) {
+    regular_expr a(5);
+    a.prefix[0] = 1;
+    a.prefix[1] = 2;
+    a.whole[2] = 2;
+    regular_expr result = clini(a, 5);
+    std::vector<int> test_prefix = {0, 2, 2, 4, 4};
+    std::vector<int> test_whole = {0, -1, 2, -1, 4};
+    ASSERT_EQ(result.prefix, test_prefix);
+    ASSERT_EQ(result.whole, test_whole);
+}
